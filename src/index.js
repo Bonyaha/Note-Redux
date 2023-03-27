0ffff289ce212feddc0aaa1431a5c19adc39975b
Roman <romchikus1987@gmail.com>
2023-03-27 12:59:26 +0300

    some changes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import noteReducer from './reducers/noteReducer'
+//import Dashboard from './App'
+import { legacy_createStore as createStore } from 'redux'
+import { Provider } from 'react-redux'
+import App from './App'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const store = createStore(noteReducer)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const renderApp = () => {
+  root.render(
+    <Provider store={store}>
+      {' '}
+      <App />
+    </Provider>
+  )
+}
+
+renderApp()
+store.subscribe(() => {
+  const storeNow = store.getState()
+  console.log(`state is this:${storeNow}`)
+})
+store.subscribe(renderApp)
