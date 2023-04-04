b5a9130b30636e4125a566db5febc79ae0b2f10d
Roman <romchikus1987@gmail.com>
2023-04-04 15:16:01 +0300

    react query library

--- src/App.js
+++ src/App.js
@@ -6,6 +6,32 @@ import { initializeNotes } from './reducers/noteReducer'
 
 import { useDispatch } from 'react-redux'
 
-const App = () => {}
+const App = () => {
+  const addNote = async (event) => {
+    event.preventDefault()
+    const content = event.target.note.value
+    event.target.note.value = ''
+    console.log(content)
+  }
+  const toggleImportance = (note) => {
+    console.log('toggle importance of', note.id)
+  }
+  const notes = []
+  return (
+    <div>
+      <h2>Notes App</h2>
+      <form onSubmit={addNote}>
+        <input name='note' />
+        <button type='submit'>add</button>
+      </form>
+      {notes.map((note) => (
+        <li key={note.id} onClick={() => toggleImportancen(note)}>
+          {note.content}
+          <strong>{note.important ? 'important' : ''}</strong>
+        </li>
+      ))}
+    </div>
+  )
+}
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Provider } from 'react-redux'
-import store from './store'
+import { QueryClient, QueryClientProvider } from 'react-query'
+
 import App from './App'
 
+const queryClient = new QueryClient()
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
+  <QueryClientProvider client={queryClient}>
+    {' '}
     <App />
-  </Provider>
+  </QueryClientProvider>
 )
